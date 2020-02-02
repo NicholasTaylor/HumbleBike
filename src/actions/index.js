4d96b8b5a6f49d9acb4cea57ea29287e93408a8e
Nick <nicholas_taylor@me.com>
2020-02-02 12:14:26 -0500

    Added favicons, deleted a few debug messages

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,6 @@ import {REQUEST_DATA_LOAD} from '../constants/action-types';
 
 export function requestDataLoad(){
 	return function(dispatch){
-		console.log(new Date());
 		return dispatch({type:REQUEST_DATA_LOAD})
 	}
 }
