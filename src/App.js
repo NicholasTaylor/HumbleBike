c55a949c900f5de6bebe25df809d72ddc9780cea
Nicholas Taylor <nicholas_taylor@me.com>
2020-10-24 13:31:57 -0400

    Added refresh button.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
       <div>
         <CustomFonts />
         <div id="container">
-          <div 
+          <div
             id="content"
             className={this.props.search + ' ' +this.props.filterElec}
           >
@@ -33,9 +33,18 @@ class App extends React.Component {
                 Last updated: {this.props.updated}
               </h2>
               <span
+                className="search btn"
+                onClick={this.props.requestDataLoad}
+              >
+                Refresh
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="btn-icon">
+                  <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
+                </svg>
+              </span>
+              <span
                 className="search"
               >
-                Search: <input 
+                Search: <input
                   type="text"
                   label="search"
                   value={this.props.searchQuery}
@@ -54,7 +63,7 @@ class App extends React.Component {
               </span>
             </div>
             {this.props.stations.map(el =>(
-              <section 
+              <section
                 key={el.station_id}
                 className={ (el.isVisible ? 'stationOn' : 'stationOff') + ' ' + (el.electric > 0 ? 'elecOn' : 'elecOff') }
               >
@@ -109,4 +118,3 @@ function mapStateToProps(state){
 
 
 export default connect(mapStateToProps,{requestDataLoad, searchStations, filterElecToggle})(App);
-
