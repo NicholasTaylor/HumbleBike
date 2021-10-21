e4da813772811af5439489cd7bab81fad19a691f
Nicholas Taylor <nicholas_taylor@me.com>
2021-10-21 00:24:35 -0400

    Transitioning elements to styled components where possible

--- frontend/src/components/Station.js
+++ frontend/src/components/Station.js
@@ -1,47 +1,102 @@
 import React from 'react';
+import { css, jsx } from '@emotion/react';
+import { fontWeight } from '../constants/style';
+/** @jsxRuntime classic */
+/** @jsx jsx */;
 
-class Station extends React.Component {
-    render(){
-        const station = this.props.data;
-        const distInfo = station.dist ? ' (' +station.dist +'mi.)' : ''
-        return(
-            <div>
-                <section
+export default function Station(props) {
+    const station = props.data;
+    const distInfo = station.dist ? ' (' +station.dist +'mi.)' : '';
+    return(
+        <div>
+            <section
                 className={(station.isVisible ? 'stationOn' : 'stationOff') + ' ' + (station.electric > 0 ? 'elecOn' : 'elecOff') + ' ' + (station.docks > 0 ? 'dockOn' : 'dockOff') + ' ' + (station.electric > 0 && station.classic === 0 ? 'elecFreeOn' : 'elecFreeOff')}
-              >
-                <div className="container-station">
-                  <h3 className="station-name">
-                    {station.name}{distInfo}
-                  </h3>
-                  <div className="infoSection classic-bikes">
-                    <h4 className="bikes-remaining">
-                      {station.classic}
-                    </h4>
-                    <h5 className="descriptor">
-                      Classic
-                    </h5>
-                  </div>
-                  <div className="infoSection ebikes">
-                    <h4 className="ebikes-remaining">
-                      {station.electric}
-                    </h4>
-                    <h5 className="descriptor">
-                      Electric
-                    </h5>
-                  </div>
-                  <div className="infoSection docks">
-                    <h4 className="docks-remaining">
-                      {station.docks}
-                    </h4>
-                    <h5 className="descriptor">
-                      Docks
-                    </h5>
-                  </div>
+            >
+                <div
+                    css={css`
+                        border-top: 1px solid rgba(0,0,0,0.25);
+                        margin: 1em 0;
+                        padding: 1em 0;
+                    `}
+                >
+                    <h3
+                        css={css`
+                            font-weight: ${fontWeight['bold']};
+                        `}
+                    >
+                        {station.name}{distInfo}
+                    </h3>
+                    <div 
+                        css={css`
+                            margin: 0;
+                            width: 29vw;
+                            display: inline-block;
+                            padding: 0;
+                            text-align: center;
+                        `}
+                        className="classic-bikes"
+                    >
+                        <h4
+                            className="bikes-remaining"
+                        >
+                            {station.classic}
+                        </h4>
+                        <h5 
+                            css={css`
+                                font-weight: ${fontWeight['light']};
+                            `}
+                        >
+                            Classic
+                        </h5>
+                    </div>
+                    <div  
+                        css={css`
+                            margin: 0;
+                            width: 29vw;
+                            display: inline-block;
+                            padding: 0;
+                            text-align: center;
+                        `}
+                        className="ebikes"
+                    >
+                        <h4 
+                            className="ebikes-remaining"
+                        >
+                            {station.electric}
+                        </h4>
+                        <h5 
+                            css={css`
+                                font-weight: ${fontWeight['light']};
+                            `}
+                        >
+                            Electric
+                        </h5>
+                    </div>
+                    <div  
+                        css={css`
+                            margin: 0;
+                            width: 29vw;
+                            display: inline-block;
+                            padding: 0;
+                            text-align: center;
+                        `}
+                        className="docks"
+                    >
+                        <h4 
+                            className="docks-remaining"
+                        >
+                            {station.docks}
+                        </h4>
+                        <h5 
+                            css={css`
+                                font-weight: ${fontWeight['light']};
+                            `}
+                        >
+                            Docks
+                        </h5>
+                    </div>
                 </div>
-              </section>
-            </div>
-        )
-    }
+            </section>
+        </div>
+    )
 }
-
-export default Station;
