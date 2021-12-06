d86dbe75836068a7864243c764c5116f11376272
Nicola Taylor <t@ylornico.la>
2021-12-06 18:37:16 -0500

    First draft of algorithm to serialize station data

--- datapull/app-protobuf-test.py
+++ datapull/app-protobuf-test.py
@@ -1,45 +1,65 @@
 import json, records_pb2, google, inspect
+from json import decoder
+from google.protobuf.json_format import MessageToJson
 
 def sort_key(e):
         return e[0]
 
 def sort_station_id(e):
-    for ele in e:
-        if ele[0] == 'station_id':
-            return int(ele[1])
+    return int(e['station_id'])
 
-def gen_sub_attr(new_inst, attr):
-    keys_list = new_inst.DESCRIPTOR.fields_by_name.keys()
+def gen_sub_attr(inst, attr):
+    keys_list = inst.DESCRIPTOR.fields_by_name.keys()
     for sub_attr in attr:
         if sub_attr not in keys_list:
             continue
         else:
-            setattr(new_inst, sub_attr, attr[sub_attr])
-    return new_inst
+            setattr(inst, sub_attr, attr[sub_attr])
+    return inst
 
-def gen_station(new_inst, station):
-    keys_list = new_inst.DESCRIPTOR.fields_by_name.keys()
+def gen_station(inst, station):
+    keys_list = inst.DESCRIPTOR.fields_by_name.keys()
     for attr in station:
         if attr not in keys_list:
             continue
         else:
             pass
-        #type_test = type(station[attr]).__name__
         attr_value = station[attr]
-        if attr == 'eightd_active_station_services' or attr == 'eightd_station_services':
+        type_test = type(attr_value).__name__
+        attr_type_test = type(getattr(inst,attr)).__name__
+        try:
+            value_dict_test = True if type(attr_value[0]).__name__ == 'dict' else False
+        except:
+            value_dict_test = False
+        if type_test == 'list' and value_dict_test:
+            attr_arr = []
+            for service in attr_value:
+                gen_sub_attr(getattr(getattr(inst, attr),'add')(), service)
+            getattr(getattr(inst, attr), 'extend')(attr_arr)
+        elif type_test == 'list':
+            getattr(getattr(inst, attr), 'extend')(attr_value)
+        elif type_test == 'dict':
+            gen_sub_attr(getattr(inst, attr), attr_value)
+        elif attr_type_test == 'Timestamp':
+            getattr(getattr(inst, attr), 'FromSeconds')(int(attr_value))
+        else:
+            setattr(inst, attr, attr_value)
+
+
+        """if attr == 'eightd_active_station_services' or attr == 'eightd_station_services':
             for service in attr_value:
                 new_inst.attr = gen_sub_attr(getattr(getattr(new_inst, attr), 'add')(), service)
         elif attr == 'rental_methods':
             getattr(getattr(new_inst, attr), 'extend')(attr_value)
         elif attr == 'rental_uris' or attr == 'valet':
-            pass
+            gen_sub_attr(getattr(getattr(new_inst, attr), 'add')(), attr_value)
+        else:
+            setattr(new_inst, attr, attr_value)"""
         
         #new_inst.attr = station[attr]
 
 def get_json(info_url, status_url):
     import requests
-    from datetime import datetime
-
     station_map = {}
     station_arr = []
     r_info = requests.get(info_url).json()['data']['stations']
@@ -50,10 +70,17 @@ def get_json(info_url, status_url):
     for station in r_status:
         full_station = station_map[station['station_id']]
         full_station.update(station)
-        full_station_arr = list(full_station.items())
-        full_station_arr.sort(key=sort_key)
-        station_arr.append(full_station_arr)
+        station_arr.append(full_station)
     station_arr.sort(key=sort_station_id)
+    return station_arr
+
+def gen_stations(inst, station_arr):
+    for station in station_arr:
+        gen_station(inst.station.add(),station)
+    return inst
+
+def gen_record(station_arr):
+    from datetime import datetime
     current = datetime.now()
     date = current.strftime('%Y-%m-%d %H:%M:%S')
     month = current.strftime('%m')
@@ -70,8 +97,6 @@ def get_json(info_url, status_url):
         'minute': minute,
         'stations': station_arr
     }
-    full_record_arr = list(full_record.items())
-    return full_record_arr
 
 def testing_view(filename):
     dict_data = load(filename, allow_pickle=True)
@@ -85,7 +110,7 @@ def testing_view(filename):
 
 """with open('output.json','w+') as f:
     f.write(json.dumps(testing_view('data.npz').tolist()))
-"""
+
 
 attr_values = {'id': 'testing123'}
 
@@ -95,6 +120,14 @@ def attr_method(new_inst, attr):
     return new_inst
 
 test = records_pb2.Station()
-indirect = 'rental_methods'
-getattr(getattr(test, indirect), 'extend')(['TESTTESTTEST'])
-print(test.rental_methods)
+indirect = 'last_reported'
+#getattr(getattr(test, indirect), 'extend')(['TESTTESTTEST'])
+print(dir(test.last_reported))
+test.last_reported.FromSeconds(int('1638808954'))
+print(test.last_reported.ToJsonString())
+"""
+
+json_out = get_json('https://gbfs.citibikenyc.com/gbfs/en/station_information.json','https://gbfs.citibikenyc.com/gbfs/en/station_status.json')
+all_stations = gen_stations(records_pb2.Stations(),json_out)
+test = json.loads(MessageToJson(all_stations))['station']
+print(test[71])
