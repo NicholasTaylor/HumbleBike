c5a4e608dcc61edd85a6eefada9ae9c0699ee5dc
Nicola Taylor <t@ylornico.la>
2022-02-05 20:36:08 -0500

    Started data conversion app. Added ability to resume datapull from latest file.

--- datapull/datapull.py
+++ datapull/datapull.py
@@ -1,3 +1,4 @@
+from gettext import find
 import json, records_pb2, os, gzip, requests
 from datetime import datetime
 
@@ -5,8 +6,11 @@ STATION_INFORMATION = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.
 STATION_STATUS = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json'
 FILE_STEM = 'records'
 FILE_PREFIX = FILE_STEM + '_'
+FILE_DIGITS_LEN = 4
+FILE_DIGITS_LEN_STR = '0' +str(FILE_DIGITS_LEN)
 FILE_EXT = '.tar.gz'
 FILE_ZERO = FILE_PREFIX + '0000' + FILE_EXT
+FILE_SETTINGS = 'settings.json'
 GIGABYTE = 1073741824
 FILE_LIMIT = 18874368
 LOG_FILE = 'log.csv'
@@ -103,13 +107,48 @@ def gen_record(record):
     gen_station(record.weather,weather_data)
     record.date.FromSeconds(int(time.mktime(datetime.now().timetuple())))
 
-def find_latest_file():
-    counter = 0
+def find_start_page_no_settings():
+	files = os.listdir('.')
+	records = []
+	for file in files:
+		prefix_len = len(FILE_PREFIX)
+		ext_len = len(FILE_EXT)
+		num_start = prefix_len
+		num_end = num_start + FILE_DIGITS_LEN
+		num_candidate = file[num_start:num_end]
+		try:
+			is_num_test = int(num_candidate)
+			is_num = True
+		except:
+			is_num = False
+		if file[0:prefix_len] == FILE_PREFIX and file[-ext_len:] == FILE_EXT and is_num:
+			records.append(int(num_candidate))
+	return max(records)
+
+def find_start_page():
+    if os.path.exists(FILE_SETTINGS):
+        try:
+            with open(FILE_SETTINGS) as f:
+                settings_dict = json.loads(f.read())
+                last_page = settings_dict['last_page']
+        except:
+            last_page = False
+    else:
+        try:
+            last_page = find_start_page_no_settings()
+        except:
+            last_page = False
+    if isinstance(last_page, int):
+        return last_page
+    else:
+        return 0
+
+def find_latest_file(current_page):
     found_file = False
     need_new_file = False
     filename = ''
     while found_file == False:
-        fn_test = FILE_PREFIX + f'{counter:04}' + FILE_EXT
+        fn_test = FILE_PREFIX + f'{current_page:{FILE_DIGITS_LEN_STR}}' + FILE_EXT
         fn_exist = True if os.path.exists(fn_test) else False
         fn_right_size = True if fn_exist and os.path.getsize(fn_test) < FILE_LIMIT else False
         if fn_exist == False:
@@ -121,22 +160,22 @@ def find_latest_file():
             need_new_file = False
             found_file = True
         else:
-            counter += 1
-    return filename, need_new_file
+            current_page += 1
+    return filename, need_new_file, current_page
 
 def save_records(records, filename):
     with gzip.open(filename, 'wb') as f:
         f.write(records.SerializeToString())
 
-def add_record():
-    filename, need_new_file = find_latest_file()
+def add_record(start_page):
+    filename, need_new_file, current_page = find_latest_file(start_page)
     records = records_pb2.Records()
     if not(need_new_file):
         with gzip.open(filename, 'rb') as f:
             records.ParseFromString(f.read())
     record = records.record.add()
     gen_record(record)
-    return records, filename
+    return records, filename, current_page
 
 def read_record():
     from google.protobuf.json_format import MessageToJson
@@ -150,8 +189,22 @@ def read_record():
         f.write(json.dumps(data_json['record']))
     return('Done')
 
-def log_run(start_time, end_time, filename):
+def log_run(start_time, end_time, filename, current_page):
     import csv
+    page_dict = {"last_page": current_page}
+    try:
+        with open(FILE_SETTINGS, 'r') as f:
+            settings_dict = json.loads(f.read())
+            old_page = settings_dict['last_page']
+    except:
+        settings_dict = {}
+        old_page = 0
+    if old_page != current_page:
+        settings_dict.update(page_dict)
+        with open(FILE_SETTINGS, 'w+') as f:
+            f.write(json.dumps(settings_dict))
+    else:
+        pass
     is_log = True if os.path.exists(LOG_FILE) else False
     diff = end_time - start_time
     duration = int(round((diff.seconds * 1000) + (diff.microseconds / 1000),0))
@@ -168,10 +221,11 @@ def log_run(start_time, end_time, filename):
 
 def datapull():
     start_time = datetime.now()
-    records, filename = add_record()
+    start_page = find_start_page()
+    records, filename, current_page = add_record(start_page)
     save_records(records, filename)
     end_time = datetime.now()
-    run_data = log_run(start_time, end_time, filename)
+    run_data = log_run(start_time, end_time, filename, current_page)
     filesize_gb = '{:.2f}'.format(run_data[LOG_FIELD_3] / GIGABYTE) if run_data[LOG_FIELD_3] > GIGABYTE/100 else '< 0.01'
     return 'Process completed at ' +end_time.strftime('%Y-%m-%d %H:%M:%S') +' in ' +str(run_data[LOG_FIELD_2]) +'ms. File ' +filename +' is now ' +filesize_gb +'GB.'
 
